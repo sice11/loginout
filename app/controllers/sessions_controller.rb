13b5338a18186d1d2dbfb418e8f9992a332b961d
sice11 <tensyoku.yonezawa@gmail.com>
2020-09-18 02:02:59 +0900

    ログイン・ログアウト機能実装完了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email])
-    if @user && @user.authenticate(params[:session][:password])
-      log_in user
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      log_in @user
       redirect_to root_path
     else
       flash.now[:danger] = "メールアドレス、またはパスワードが異なります。"
